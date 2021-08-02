acfaffbcb9d5644e37579167e1d990655783dfb3
kareem693 <31923015+kareem693@users.noreply.github.com>
2021-08-02 14:02:29 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World----");
-    System.out.println("Github Data is Cloned and Compiled on jenkins dashboard")
+    System.out.println("Github Data is Cloned and Compiled on jenkins dashboard");
       
   }
 }
